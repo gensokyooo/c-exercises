6ebf4a9986f003db9ad83435a1ea9e5984f82ac6
gensokyoz1 <gensokyochen29@gmail.com>
2026-04-23 16:49:59 +0200

    Modified rotate linked list

--- datastructures/linkedlists/04_rotate.cpp
+++ datastructures/linkedlists/04_rotate.cpp
@@ -6,45 +6,28 @@ struct node {
     node* next;
 };
 
-//Write a function named rotate that takes a singly linked list lst (with integer data)
-//as a parameter and performs a "forward rotation."
-//The rotation moves the last element of the list to the head position.
-//For example, if lst = 1 -> 2 -> 3 -> 4 -> 5,
-//after calling rotate(lst), the list becomes lst = 5 -> 1 -> 2 -> 3 -> 4.
-//The function must appropriately handle empty lists.
-//Additionally, define the node structure for a singly linked list containing integer data.
+void rotate (node*& lst) {
+    if (!lst || !lst->next) return;
+    node* prev = nullptr;
+    node* curr = lst;
 
-
-void rotate (node*& lst1) {
-
-    if (lst1 == nullptr || lst1->next == nullptr) return;
-
-    node* curr = lst1;
-
-    while (curr->next->next != nullptr) {
+    while (curr->next != nullptr) {
+        prev = curr;
         curr = curr->next;
     }
-    node* last = curr->next;
-    curr->next = nullptr;
-    last->next = lst1;
-    lst1 = last;
-}
 
-void print(node* lst1) {
-    node* curr = lst1;
-    while (curr != nullptr) {
-        cout << curr->info << endl;
-        curr = curr->next;
-    }
+    prev->next = nullptr;
+    // 1 2 -> nullptr
+    curr->next = lst;
+    // 3->1->2->nullptr
+    lst = curr;
 }
-
 int main () {
-    node* c = new node { 1, nullptr};
-    node* b = new node { 2, c};
-    node* a = new node {5, b};
 
-    node* lst1 = a;
+    node* c = new node {3, nullptr};
+    node* b = new node {2,c};
+    node* a = new node {1,b};
 
-    rotate(lst1);
-    print(lst1);
+    node* lst = a;
+    rotate(lst);
 }
