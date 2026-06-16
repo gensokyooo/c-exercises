15760920a7b649ad6da199bf81881b7d97098708
gensokyoz1 <gensokyochen29@gmail.com>
2026-06-16 13:27:25 +0200

    Finished Map Exercise

--- Object Oriented Programming/04_Map.cpp
+++ Object Oriented Programming/04_Map.cpp
@@ -3,9 +3,35 @@ using namespace std;
 
 template <typename K, typename V>
 class Map {
+private:
     K key [100];
     V value [100];
     int count;
+    int capacity;
+public:
+    Map () {
+        capacity = 100;
+        count = 0;
+    }
+    void insert (const K& key, const V& value) {
+            // We don't check for capacity here because we're only updating a value
+            for (int i = 0; i < count; i ++ ) {
+                if (key == this->key[i]) {
+                    this->value[i] = value;
+                    // We return because asap you found the value you must not continue
+                    // adding (like going to the code right outside the loop)
+                    return;
+                }
+            }
+
+            if (count < capacity) {
+            this->key[count] = key;
+            this->value[count] = value;
+            count++;
+        } else {
+            throw runtime_error ("Capacity reached");
+        }
+    }
 };
 
 int main () {
