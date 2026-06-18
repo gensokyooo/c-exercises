86410fa8106fddb714299b6e72be31bdc2dc1b0f
gensokyoz1 <gensokyochen29@gmail.com>
2026-06-18 21:44:10 +0200

    queue

--- Object Oriented Programming/11_ QueuePractice.cpp
+++ Object Oriented Programming/11_ QueuePractice.cpp
@@ -4,11 +4,29 @@ using namespace std;
 
 template <typename T>
 class Queue {
+private:
     T* array;
     int capacity;
     int count;
 
+public:
+    Queue () {
+        capacity = 100;
+        count = 0;
+        array = new T[capacity];
+    }
 
+    void resize () {
+        int newCapacity = capacity*2;
+        // Why create a new array? Because we need something to fill in the elements from last array
+        T* newArray = new T[newCapacity];
+        for (int i = 0; i < count; i++) {
+            newArray[i] = array[i];
+        }
+        delete[] array;
+        array = newArray;
+        capacity = newCapacity;
+    }
 
 };
 
