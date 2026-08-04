7e053733911bd609a489547af646d03b435a3cc1
gensokyoz1 <gensokyochen29@gmail.com>
2026-08-04 12:07:03 +0200

    stuff

--- strings/shiftleft.cpp
+++ strings/shiftleft.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 using namespace std;
 
-void shift_left (int arr[], int n, int k) {
+
+struct node {
+    int info;
+    node* next;
+
+};
+
+node* sumlists (node* lst1, node* lst2) {
+
+    node* curr2 = lst2;
+    node* curr = lst1;
+    node* head = nullptr;
+    node* tail = nullptr;
+
+
+
+
+    while (curr != nullptr && curr2 != nullptr) {
+        int result = curr->info + curr2->info;
+
+        node* newNode = new node {result,nullptr};
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        curr = curr->next;
+        curr2 = curr2->next;
+    }
+
+
 
 }
-
-
-
 
 int main () {
 
