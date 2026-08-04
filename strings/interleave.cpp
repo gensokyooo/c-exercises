1271d71604a57f8ceb694182d0e7fd243a69e98b
gensokyoz1 <gensokyochen29@gmail.com>
2026-08-04 18:05:24 +0200

    Gensokyo Solutions Baby

--- strings/interleave.cpp
+++ strings/interleave.cpp
@@ -22,11 +22,55 @@ char* interleave (char* str1, char* str2) {
     newStr[temp] = '\0';
     return newStr;
 
+}
+
+struct node {
+    int info;
+    node* next;
+};
+
+void rotate_back (node*& lst1) {
+
+    if (lst1 == nullptr) {
+        return;
+    }
+
+    node* first_element = lst1;
+    node* curr = lst1;
+    while (curr->next != nullptr) {
+        curr = curr->next;
+    }
+
+    curr->next = first_element;
+    // so now: 1 2 3 4 5 -> 1 2 3 4 5
+    //         ^ lst1
+    lst1 = lst1->next;
+    // now:
+    // 2 -> 3 -> 4 -> 5 -> 1
+    // ^lst1
+    first_element->next = nullptr;
+    // now:
+    // 2- > 3 -> 4 -> 5 -> 1 -> nullptr
 
 }
 
+#define MAX 101
 int main () {
 
+char str[MAX];
+char str2[MAX];
 
+    cin.getline(str,MAX);
+    if (cin.fail()) {
+        return -1;
+    }
+    cin.getline(str2,MAX);
+    if (cin.fail()) {
+        return -1;
+    }
+
+    char* result = interleave (str,str2);
+    cout << result;
 
 }
+
