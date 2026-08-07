62f39d19d25a09f938a6a81c2d72d2fad88fbb18
gensokyoz1 <gensokyochen29@gmail.com>
2026-08-07 12:04:39 +0200

    Yep Gensokyo Solutions sir

---
+++ strings/hasZeroSum.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+bool hasZeroSum (int arr[], int length) {
+
+
+    for (int i = 0; i < length; i ++) {
+        int sum = 0;
+        bool match = false;
+        for (int j = i; j < length; j++) {
+            sum = arr[j]+sum;
+            if (sum == 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+char* removeDuplicates (char* str) {
+
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    char* newStr = new char[length+1];
+    int temp = 0;
+    for (int i = 0; i < length; i++) {
+            int count = 0;
+        for (int j = 0; j < length; j++) {
+            if (str[i] == str[j]) {
+                count++;
+            }
+        }
+        if (count == 1) {
+            newStr[temp] = str[i];
+            temp++;
+        }
+    }
+
+newStr[temp] = '\0';
+return newStr;
+}
+
+int main () {
+
+}
