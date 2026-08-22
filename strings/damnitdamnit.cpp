51d85810de5103407e579532c8ccad52df264e83
gensokyoz1 <gensokyochen29@gmail.com>
2026-08-22 13:39:20 +0200

    Yes, yes!

--- strings/damnitdamnit.cpp
+++ strings/damnitdamnit.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int count_increasing_triples (int arr[], const int n) {
-
+    int count = 0;
+    for (int i = 0; i < n; i ++) {
+        for (int j = i+i; j < n; j++) {
+            for (int l = j+1; l < n; l++) {
+                if (arr[i] < arr[j] && arr[j] < arr[l]) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
 
 }
 
